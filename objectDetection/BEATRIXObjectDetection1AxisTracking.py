1cb1f138e0fb133759ff52da3a1317332baffcd6
inte-R-action <u.marherz@gmail.com>
2023-08-24 01:08:02 +0100

    first example of ball detection and simple tracking in 1 axis
    Co-Authored-By: Uriel Martinez <4717988+urielmtz@users.noreply.github.com>

---
+++ objectDetection/BEATRIXObjectDetection1AxisTracking.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 01:03:44 2023
+
+@author: umh21
+"""
+
+# steps
+# 1 - connect BEATRIX to COM port
+# 2 - manually define the home position
+# 3 - enable motors
+# 4 - calibrate motors to set the home position
+# 5 - connect the camera (only left or right camera)
+# 6 - enable ball detection
+# 7 - enable tracking the ball in 1 axis at the time
+
